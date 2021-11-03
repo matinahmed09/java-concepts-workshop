d7611a99aafe6e5a0e96556b0ccaef718031c449
Matin Work Pc <abdul-matin.ahmed@turntabl.io>
2021-11-03 14:56:19 +0000

    Changes made

--- overloading/src/Addition.java
+++ overloading/src/Addition.java
@@ -6,5 +6,15 @@
 
 public class Addition {
 
+    public int add (int a, int b){
+        return(a+b);
+    }
+    public float add(float a, float b, float c){
+        return (a + b+ c);
+    }
+    public String add(String a, String b){
+        return (a + b);
+    }
+
 }
 
